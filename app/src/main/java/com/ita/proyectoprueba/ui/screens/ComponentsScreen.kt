ddc6ef83b2b5d989904e8d6784e6c30d4b62ba1d
diazd <diazdeleondolores12@gmail.com>
2024-09-30 08:32:29 -0600

    Components-Progress,Sliders,Swithces

--- app/src/main/java/com/ita/proyectoprueba/ui/screens/ComponentsScreen.kt
+++ app/src/main/java/com/ita/proyectoprueba/ui/screens/ComponentsScreen.kt
@@ -3,14 +3,21 @@ package com.example.project1.ui.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.AssistChip
 import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExtendedFloatingActionButton
@@ -19,12 +26,17 @@ import androidx.compose.material3.FilterChip
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.InputChip
+import androidx.compose.material3.InputChipDefaults
 import androidx.compose.material3.LargeFloatingActionButton
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Slider
 import androidx.compose.material3.SmallFloatingActionButton
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDrawerState
@@ -34,8 +46,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.structuralEqualityPolicy
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -55,8 +69,7 @@ fun ComponentsScreen(navController: NavController) {
                 Text("Menu", modifier = Modifier.padding(16.dp))
                 //Content 1
                 HorizontalDivider()
-                NavigationDrawerItem(
-                    label = { Text(text = "Content 1") },
+                NavigationDrawerItem(label = { Text(text = "Content 1") },
                     selected = false,
                     onClick = {
                         component = "Content 1"
@@ -65,12 +78,10 @@ fun ComponentsScreen(navController: NavController) {
                                 close()
                             }
                         }
-                    }
-                )
+                    })
 
                 //Content 2
-                NavigationDrawerItem(
-                    label = { Text(text = "Content 2") },
+                NavigationDrawerItem(label = { Text(text = "Content 2") },
                     selected = false,
                     onClick = {
                         component = "Content 2"
@@ -79,12 +90,10 @@ fun ComponentsScreen(navController: NavController) {
                                 close()
                             }
                         }
-                    }
-                )
+                    })
 
                 //Buttons
-                NavigationDrawerItem(
-                    label = { Text(text = "Buttons") },
+                NavigationDrawerItem(label = { Text(text = "Buttons") },
                     selected = false,
                     onClick = {
                         component = "Buttons"
@@ -93,12 +102,10 @@ fun ComponentsScreen(navController: NavController) {
                                 close()
                             }
                         }
-                    }
-                )
+                    })
 
                 //FloatingButtons
-                NavigationDrawerItem(
-                    label = { Text(text = "FloatingButtons") },
+                NavigationDrawerItem(label = { Text(text = "FloatingButtons") },
                     selected = false,
                     onClick = {
                         component = "FloatingButtons"
@@ -107,25 +114,55 @@ fun ComponentsScreen(navController: NavController) {
                                 close()
                             }
                         }
-                    }
-                )
+                    })
 
                 //Chips
-                NavigationDrawerItem(
-                    label = { Text(text = "Chips") },
+                NavigationDrawerItem(label = { Text(text = "Chips") }, selected = false, onClick = {
+                    component = "Chips"
+                    scope.launch {
+                        drawerState.apply {
+                            close()
+                        }
+                    }
+                })
+
+                //Progress
+                NavigationDrawerItem(label = { Text(text = "Progress") },
                     selected = false,
                     onClick = {
-                        component = "Chips"
+                        component = "Progress"
                         scope.launch {
                             drawerState.apply {
                                 close()
                             }
                         }
-                    }
-                )
+                    })
+
+                //Sliders
+                NavigationDrawerItem(label = { Text(text = "Sliders") },
+                    selected = false,
+                    onClick = {
+                        component = "Sliders"
+                        scope.launch {
+                            drawerState.apply {
+                                close()
+                            }
+                        }
+                    })
+
+                //Switches
+                NavigationDrawerItem(label = { Text(text = "Switches") },
+                    selected = false,
+                    onClick = {
+                        component = "Switches"
+                        scope.launch {
+                            drawerState.apply {
+                                close()
+                            }
+                        }
+                    })
             }
-        }
-    ) {
+        }) {
         //ScreenContent
         Column {
             when (component) {
@@ -148,6 +185,18 @@ fun ComponentsScreen(navController: NavController) {
                 "Chips" -> {
                     Chips()
                 }
+
+                "Progress" -> {
+                    Progress()
+                }
+
+                "Sliders" -> {
+                    Sliders()
+                }
+
+                "Switches" -> {
+                    Switches()
+                }
             }
         }
     }
@@ -156,16 +205,14 @@ fun ComponentsScreen(navController: NavController) {
 @Composable
 fun Content1() {
     Text(
-        text = "Content 1", modifier = Modifier
-            .padding(0.dp, 30.dp, 0.dp, 0.dp)
+        text = "Content 1", modifier = Modifier.padding(0.dp, 30.dp, 0.dp, 0.dp)
     )
 }
 
 @Composable
 fun Content2() {
     Text(
-        text = "Content 2", modifier = Modifier
-            .padding(0.dp, 30.dp, 0.dp, 0.dp)
+        text = "Content 2", modifier = Modifier.padding(0.dp, 30.dp, 0.dp, 0.dp)
     )
 }
 
@@ -174,8 +221,7 @@ fun Buttons() {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceEvenly,
-        modifier = Modifier
-            .fillMaxSize()
+        modifier = Modifier.fillMaxSize()
     ) {
         Button(onClick = {}) {
             Text(text = "Filled")
@@ -204,8 +250,7 @@ fun FloatingButtons() {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceEvenly,
-        modifier = Modifier
-            .fillMaxSize()
+        modifier = Modifier.fillMaxSize()
     ) {
 
         FloatingActionButton(onClick = {}) {
@@ -220,11 +265,9 @@ fun FloatingButtons() {
             Icon(Icons.Filled.Add, contentDescription = "")
         }
 
-        ExtendedFloatingActionButton(
-            onClick = {},
+        ExtendedFloatingActionButton(onClick = {},
             icon = { Icon(Icons.Filled.Add, contentDescription = "") },
-            text = { Text(text = "Extended FAB") }
-        )
+            text = { Text(text = "Extended FAB") })
     }
 }
 
@@ -234,38 +277,154 @@ fun Chips() {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceEvenly,
-        modifier = Modifier
-            .fillMaxSize()
+        modifier = Modifier.fillMaxSize()
     ) {
-        AssistChip(
-            onClick = {},
-            label = { Text(text = "Assist Chip") },
-            leadingIcon = {
-                Icon(
-                    Icons.Filled.AccountBox, contentDescription = "",
-                    modifier = Modifier
-                        .size(AssistChipDefaults.IconSize)
-                )
-            }
-        )
+        AssistChip(onClick = {}, label = { Text(text = "Assist Chip") }, leadingIcon = {
+            Icon(
+                Icons.Filled.AccountBox,
+                contentDescription = "",
+                modifier = Modifier.size(AssistChipDefaults.IconSize)
+            )
+        })
 
         var selected by remember { mutableStateOf(false) }
 
-        FilterChip(
-            selected = selected,
+        FilterChip(selected = selected,
             onClick = { selected = !selected },
             label = { Text(text = "Filter Chip") },
             leadingIcon = if (selected) {
                 {
                     Icon(
-                        Icons.Filled.AccountBox, contentDescription = "",
-                        modifier = Modifier
-                            .size(AssistChipDefaults.IconSize)
+                        Icons.Filled.AccountBox,
+                        contentDescription = "",
+                        modifier = Modifier.size(AssistChipDefaults.IconSize)
                     )
                 }
-            } else{
+            } else {
                 null
-            }
+            })
+    }
+
+    InputChipExample(text = "Dismiss", {})
+
+}
+
+@Composable
+fun InputChipExample(
+    text: String, onDismiss: () -> Unit
+) {
+    var enabled by remember { mutableStateOf(true) }
+
+    if (!enabled) return
+
+    InputChip(label = { Text(text) }, selected = enabled, onClick = {
+        onDismiss()
+        enabled = !enabled
+    },
+
+        avatar = {
+            Icon(
+                Icons.Filled.Person,
+                contentDescription = "",
+                Modifier.size(InputChipDefaults.AvatarSize)
+
+            )
+        },
+
+        trailingIcon = {
+            Icon(
+                Icons.Filled.Close,
+                contentDescription = "",
+                Modifier.size(InputChipDefaults.AvatarSize)
+
+            )
+        })
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Progress() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        LinearProgressIndicator(
+            modifier = Modifier.fillMaxWidth()
         )
+
+        CircularProgressIndicator(
+            modifier = Modifier.width(64.dp)
+        )
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Sliders() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        var sliderPosition by remember { mutableStateOf(50f) }
+
+        Column() {
+            Slider(
+                value = sliderPosition,
+                onValueChange = { sliderPosition = it },
+                steps = 10,
+                valueRange = 0f..100f
+            )
+
+            Text(
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth(),
+                text = sliderPosition.toString()
+            )
+
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Switches() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        var checked by remember { mutableStateOf(true) }
+
+        Switch(checked = checked, onCheckedChange = {
+            checked = it
+        })
+
+        var checked2 by remember { mutableStateOf(true) }
+
+        Switch(checked = checked2, onCheckedChange = {
+            checked2 = it
+        }, thumbContent = if (checked2) {
+            {
+                Icon(
+                    Icons.Filled.Check,
+                    contentDescription = "",
+                    Modifier.size(InputChipDefaults.AvatarSize)
+
+                )
+            }
+        } else {
+            null
+        })
+
+        var checked3 by remember { mutableStateOf(true) }
+        Checkbox(checked = checked3, onCheckedChange = { checked3 = it })
+
     }
 }
